acc2f6eff0a4a5bc9b2b48849ee7d525f4061734
thisme8 <stutiupreti8@gmail.com>
2025-11-18 11:56:15 +0545

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const primary = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const secondary = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "URL Shortener Frontend",
+  description: "Created the frontend for the URL Shortener Project",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${primary.variable} ${secondary.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  )
+}
