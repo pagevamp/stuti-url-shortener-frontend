b1d20350145504226bb5730a04bbe809689d312e
thisme8 <stutiupreti8@gmail.com>
2025-11-24 13:39:05 +0545

    feat(auth) : Resolve comments on PR #2

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,8 +9,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <div className="relative">
       <RadialDecorator />
       <section className="max-w-screen h-screen grid grid-cols-2 gap-10 mx-10 my-10 px-20 absolute top-0">
-        <ImageSlider />
-        <div className="bg-(--color-undraw-primary-100) h-[90%] rounded-3xl shadow-2xl shadow-lime-950 ">
+        <section className="hidden lg:block">
+          {" "}
+          <ImageSlider />
+        </section>
+        <div className="bg-(--color-undraw-primary-100) h-[90%] rounded-3xl shadow-2xl shadow-lime-950 md:w-[600px] sm:min-w-[400px] xs:min-w-[200px]">
           <Container>{children}</Container>
         </div>
       </section>
