e2e87ea3079a9f0bdaa3ea2dd1fac1cf3673f6d5
thisme8 <stutiupreti8@gmail.com>
2025-12-01 06:50:04 +0545

    Fix(feat/auth-token): Fix api import

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,27 +1,37 @@
 import axios from 'axios';
 import { getCookie } from './actions';
 
-const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
-  timeout: 5000,
-});
+const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
 
-api.interceptors.request.use(
-  async (config) => {
-    if (config.url?.includes('/urls, /url-analytics'))
-      try {
-        const accessToken = getCookie();
-        if (accessToken && config.headers) {
-          config.headers['Authorization'] = `Bearer ${accessToken}`;
+const createApiInstance = (baseURL: string | undefined) => {
+  const instance = axios.create({
+    baseURL,
+    timeout: 5000,
+    withCredentials: true,
+  });
+
+  instance.interceptors.request.use(
+    async (config) => {
+      if (
+        config.url?.includes('/urls, /url-analytics') &&
+        typeof window === 'undefined'
+      )
+        try {
+          const accessToken = getCookie();
+          if (accessToken && config.headers) {
+            config.headers['Authorization'] = `Bearer ${accessToken}`;
+          }
+        } catch (err) {
+          console.error('Invalid token', err);
         }
-      } catch (err) {
-        console.error('Invalid token', err);
-      }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
 
-export default api;
+  return instance;
+};
+
+export const api = createApiInstance(BASE_API_URL);
