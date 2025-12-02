be0af43b3c377b33c6d7d0bc4968b44f32bed441
thisme8 <stutiupreti8@gmail.com>
2025-12-02 10:01:21 +0545

    fix(feat/auth-token)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,27 @@
-import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
-import "./globals.css"
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import './colors.css';
 
 const primary = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
 
 const secondary = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
-  title: "URL Shortener Frontend",
-  description: "Created the frontend for the URL Shortener Project",
-}
+  title: 'URL Shortener Frontend',
+  description: 'Created the frontend for the URL Shortener Project',
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -28,5 +29,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-  )
+  );
 }
