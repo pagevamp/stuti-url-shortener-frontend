3e1d8a990e49f81f2aada6869dbd3a2c28ce2666
thisme8 <stutiupreti8@gmail.com>
2025-12-05 10:54:17 +0545

    fix(feat/url-tables):add button component

--- src/components/common/ConfirmationBox/ConfirmationDialogBox.tsx
+++ src/components/common/ConfirmationBox/ConfirmationDialogBox.tsx
@@ -18,6 +18,8 @@ export const ConfirmationDialogBox = ({
     <article className="absolute top-[35%] left-[35%] mx-auto">
       <section className="flex flex-col items-center gap-2 border border-undraw-secondary-100 shadow-xl shadow-blue-950 rounded-2xl bg-white px-10 py-12 relative z-100 w-fit">
         <Button
+          variant="icon"
+          size="icon"
           className="absolute -top-1 -right-1 py-1 px-2 bg-white text-red-700 border border-red-700 rounded-full cursor-pointer text-xs shadow-2xl"
           aria-label="Close"
           onClick={onCancel}
@@ -31,13 +33,13 @@ export const ConfirmationDialogBox = ({
 
         <div className="flex flex-row gap-4">
           <Button
-            className="text-xs font-bold text-white bg-red-950 border-0 rounded-2xl p-4"
+            className="text-xs font-bold text-white bg-red-950 border-0 rounded-2xl p-4 w-fit"
             onClick={onCancel}
           >
             Cancel
           </Button>
           <Button
-            className="text-xs font-bold text-white bg-blue-950 border-0 rounded-2xl p-4"
+            className="text-xs font-bold text-white bg-blue-950 border-0 rounded-2xl p-4 w-fit"
             type="submit"
             onClick={onConfirm}
           >
